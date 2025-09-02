be4705a6a111adb9ad6277d94132dcd6fa862289
Sohana Saeed <sohanasaeed61@gmail.com>
2025-09-02 19:34:16 +0500

    hogya nhi

--- index.py
+++ index.py
@@ -1,1 +1,28 @@
-hjcs
+# message = ""
+
+# while message.lower()!= "bye":
+#     message =input("You: ")
+#     if message.lower()!= "bye":
+#         print("chatbot: I hear you said", message)
+
+# print("chatbot: Byee dear apna khayal rkhna..")
+
+# distance = 0
+
+# while distance<11:
+#     print("Distance:", distance, "km")
+#     distance+=1
+# print("Trip end")
+
+# rows = ["A", "B", "C"]
+
+# for row in rows:
+#     for seat in range(1,4):
+#         print(row + str(seat) )
+
+
+for i in range(3):
+    for j in range(3):
+        print("X")
+    
+
