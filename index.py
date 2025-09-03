4d6d7c9a2367e73daa5bdcd89f0722bcab11675d
Sohana Saeed <sohanasaeed61@gmail.com>
2025-09-03 21:58:59 +0500

    new change

--- index.py
+++ index.py
@@ -20,13 +20,60 @@
 #     for seat in range(1,4):
 #         print(row + str(seat) )
 
-total = 0
-num = int(input("Enter your number: "))
-while num!=0:
-    total+=num
-    num = int(input("Enter your number: "))
+# total = 0
+# num = int(input("Enter your number: "))
+# while num!=0:
+#     total+=num
+#     num = int(input("Enter your number: "))
 
-print("total sum is:", total)
+# print("total sum is:", total)
+# correct_password = "sohana"
+# password = ""
+# while password!= correct_password:
+#     password = input("Enter your password: ")
+#     if password!= correct_password:
+#         print("Invalid password, Try again")
 
+# print("unlocked successfully")
+
+
+# factorial = int(input("Enter your number: "))
+# i = factorial
+# fact = 1
+
+# while i > 0:
+#     fact=fact *i
+#     i-=1
+
+# print("The factorial is:",fact)
+
+# num = int(input("Enter your number: "))
+# limit = int(input("Enter limit number: "))
+
+# i = 0
+# while i<(limit+1):
+#     mul = i*num
+#     i+=1
+
+# print("The multiplication is:", mul)
+
+# total_even = 0
+# num = int(input("Enter number (-1 to stop): "))
+
+# while num != -1:
+#     if num % 2 == 0:
+#         total_even += num
+#     num = int(input("Enter number (-1 to stop): "))
+
+# print("Sum of even numbers =", total_even)
+
+Total_sum = 0
+num = int(input("Enter your number (-1 to stop): "))
+while num!= -1:
+    if num%2==0:
+        Total_sum+=num
     
+    num = int(input("Enter your number (-1 to stop): "))
+
+print("the sum of even number:", Total_sum)
 
